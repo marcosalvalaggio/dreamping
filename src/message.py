1e4a9a86f6d88793f73c74401023422fc931390f
marcosalvalaggio <mar.salvalaggio@gmail.com>
2024-01-05 11:07:58 +0100

    wip

--- src/message.py
+++ src/message.py
@@ -1,11 +1,16 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QDialog, QLineEdit, QDialogButtonBox, QHBoxLayout
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QDialog, QLineEdit, QDialogButtonBox, QHBoxLayout, QLabel
 
 class SMTPConfig(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("SMTP Config")
         self.setGeometry(100, 100, 400, 100)
-        self.folder_path = None
+        
+        self.smtp_server = ""
+        self.smtp_port = ""
+        self.sender_email = ""
+        self.sender_password = ""
+        self.reciver_email = ""
 
         layout = QHBoxLayout()
 
@@ -20,13 +25,36 @@ class SMTPConfig(QWidget):
     def show_dialog(self):
         dialog = QDialog(self)
         dialog.setWindowTitle("SMTP Config Data")
-        dialog.setGeometry(200, 200, 300, 100)
+        dialog.setGeometry(400, 400, 400, 400)
 
         layout = QVBoxLayout()
 
-        config_textbox = QLineEdit()
-        config_textbox.setPlaceholderText("Enter Config Data")
-        layout.addWidget(config_textbox)
+        smtp_server_input = QLineEdit()
+        smtp_server_input.setPlaceholderText("Enter SMTP Server name")
+        self.smtp_server = smtp_server_input.text()
+
+        smtp_port_input = QLineEdit()
+        smtp_port_input.setPlaceholderText("Enter SMTP Port number")
+        self.smtp_port = smtp_port_input.text()
+
+        sender_email_input = QLineEdit()
+        sender_email_input.setPlaceholderText("Enter Sender Email")
+        self.sender_email = sender_email_input.text()
+        
+        sender_password_input = QLineEdit()
+        sender_password_input.setPlaceholderText("Enter Sender Password")
+        self.sender_password = sender_password_input.text()
+
+        reciver_email_input = QLineEdit()
+        reciver_email_input.setPlaceholderText("Enter Reciver Email")
+        self.reciver_email = reciver_email_input.text()
+
+        layout.addWidget(smtp_server_input)
+        layout.addWidget(smtp_port_input)
+        layout.addWidget(sender_email_input)
+        layout.addWidget(sender_password_input)
+        layout.addWidget(reciver_email_input)
+        
 
         button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         button_box.accepted.connect(dialog.accept)
@@ -38,7 +66,7 @@ class SMTPConfig(QWidget):
         result = dialog.exec_()
 
         if result == QDialog.Accepted:
-            config_data = config_textbox.text()
+            config_data = [self.smtp_server, self.smtp_port, self.sender_email, self.sender_password, self.reciver_email]
             # You can use the config_data as needed
             print("Config Data:", config_data)
 
